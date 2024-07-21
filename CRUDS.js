a820554f3b34da5cd7101e5d172a04271d58237f
Mohamed27wadi3 <ghrib27wadi@gmail.com>
2024-07-21 23:08:03 +0100

    CRUDS

--- CRUDS.js
+++ CRUDS.js
@@ -7,7 +7,7 @@ let total =document.getElementById('total');
 let count =document.getElementById('count');
 let category =document.getElementById('category');
 let submit =document.getElementById('submit');
-console.log(category);
+
 //get total 
 function getTotal() {
 if(price.value != ''){
@@ -20,5 +20,103 @@ if(price.value != ''){
     total.innerHTML = 'Please enter a valid price';
     total.style.background='red';
 }
+}
+
+// creat product 
+let dataPro;
+if(localStorage.product != null){
+    dataPro= JSON.parse(localStorage.product)
+} 
+else {
+    dataPro =[];
+}
+submit.onclick = function(){
+    let newPro = {
+        title: title.value,
+        price: +price.value,
+        taxes: +taxes.value,
+        ads: +ads.value,
+        count: +count.value,
+        discount: +discount.value,
+        total: +total.innerHTML,
+        category: category.value,
+    }
+    if (newPro.count > 1){
+        for (let i = 0 ; i < newPro.count ; i++){
+        dataPro.push(newPro);   }
+       
+       }
+    else{        dataPro.push(newPro);
+}
+
+
+// save localStorage
+localStorage.setItem('product',JSON.stringify(dataPro)  )
+
+clearData()
+showData()
+}
+
+//clear input 
+
+function clearData() {
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    count.value = '';
+    discount.value = '';
+    total.innerHTML = '';
+    category.value = '';
+}
+
+
+ // read 
+ function showData(){
+    let table = ''; 
+for (let i=0 ; i<dataPro.length ; i++) {
+table+=`
+<tr>
+        <td>${i}</td>
+        <td>${dataPro[i].title}</td>
+        <td>${dataPro[i].price}</td>
+        <td>${dataPro[i].taxes}</td>
+        <td>${dataPro[i].ads}</td>
+        <td>${dataPro[i].discount}</td>
+        <td>${dataPro[i].total}</td>
+        <td>${dataPro[i].category}</td>
+        <td id="update"><button id"update" onclick="updateData(${i})"   >update</button></td>
+        <td><button id"delet" onclick="deleteData(${i})">Delete</button></td>
+
+</tr>
+        `
+    }
+
+ document.getElementById('tbody').innerHTML=table
+ let btnDelete=document.getElementById('deleteALL');
+if(dataPro.length > 0 ){
+btnDelete.innerHTML=`<button onclick="deleteAll()" id="deletAll">DeleteAll  ( ${dataPro.length} ) </button> `
+}else{
+    btnDelete.innerHTML='';
+}
 
 }
+showData()
+
+
+// delete
+
+function deleteData(index) {
+    dataPro.splice(index, 1);
+    localStorage.setItem('product', JSON.stringify(dataPro));
+    showData();
+} 
+// delete all
+function deleteAll() {
+    localStorage.clear()
+    dataPro.splice(0)
+    showData()
+}
+
+
+
